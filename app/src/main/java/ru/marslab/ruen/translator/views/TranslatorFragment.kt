ff83d2d1956f1995b7df20e1260aa051dff4e9e5
phil <flip.04@mail.ru>
2022-04-08 11:12:10 +0300

    buttons style

--- app/src/main/java/ru/marslab/ruen/translator/views/TranslatorFragment.kt
+++
@@ -1,8 +0,0 @@
-package ru.marslab.ruen.translator.views
-
-import ru.marslab.ruen.databinding.FragmentTranslatorBinding
-import ru.marslab.ruen.view.ViewBindingFragment
-
-class TranslatorFragment :
-    ViewBindingFragment<FragmentTranslatorBinding>(FragmentTranslatorBinding::inflate) {
-}
